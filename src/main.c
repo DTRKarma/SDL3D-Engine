5eef708e71f3ebb6fded9782f33c63bf0e3a01b7
Hamza El Hadouchi <hamza.elhadouchi95@gmail.com>
2026-09-15 15:37:53 +0200

    Modified main file to make it work with a buffer ! (not working need to be fixed)

---
+++ src/main.c
@@ -0,0 +1,74 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define WINDOW_HEIGHT 450
+#define WINDOW_WIDTH 800
+#define WINDOW_TOTAL_PIXELS (WINDOW_WIDTH * WINDOW_HEIGHT)
+
+void	color_buffer(uint32_t **buffer, uint32_t color)
+{
+	for (int i = 0; i < WINDOW_TOTAL_PIXELS; i++)
+		*buffer[i] = color;
+}
+
+int	main(void) 
+{
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("Erreur lors de l'initialisation de SDL : %s", SDL_GetError());
+        return (1);
+    }
+
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+
+    if (!SDL_CreateWindowAndRenderer("SDL3D", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer))
+    {
+        SDL_Log("Erreur lors de la création de la fenêtre et du renderer : %s", SDL_GetError());
+        SDL_Quit();
+        return (1);
+    }
+
+    int running = 1;
+    SDL_Event event;
+    
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    uint32_t *buffer = malloc(WINDOW_TOTAL_PIXELS * sizeof(uint32_t));
+
+    if (!buffer)
+    {
+	    printf("Malloc failed");
+	    return (1);
+    }
+
+    SDL_UpdateTexture(texture, NULL, buffer, WINDOW_WIDTH * sizeof(uint32_t));
+
+    while (running) 
+    {
+        while (SDL_PollEvent(&event)) 
+	{
+            if (event.type == SDL_EVENT_QUIT) 
+                running = 0;
+        }
+
+        SDL_RenderClear(renderer);
+
+	color_buffer(&buffer, 0xFF0000FF);
+
+    	SDL_UpdateTexture(texture, NULL, buffer, WINDOW_WIDTH * sizeof(uint32_t));
+	SDL_RenderTexture(renderer, texture, NULL, NULL);
+        
+	SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
